017bbd184ed90e2caed232553883206c3c444e47
Abruccii <176839680+Abruccii@users.noreply.github.com>
2024-12-02 19:26:29 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export { PSTFile } from './PSTFile.class'
+
+export { PSTTask } from './PSTTask.class'
+
+export { PSTFolder } from './PSTFolder.class'
+
+export { PSTRecipient } from './PSTRecipient.class'
+
+export { PSTMessage } from './PSTMessage.class'
+export { PSTContact } from './PSTContact.class'
+export { PSTAppointment } from './PSTAppointment.class'
+
+export { PSTAttachment } from './PSTAttachment.class'
+export { PSTNodeInputStream } from './PSTNodeInputStream.class'
